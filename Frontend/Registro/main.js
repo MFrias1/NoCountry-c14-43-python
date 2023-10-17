f5c70ec164e75110500b56d6a6fafd72788865f8
mayra <m.frias09@hotmail.com>
2023-10-17 13:50:13 -0300

    nuevo

--- Frontend/Registro/main.js
+++ Frontend/Registro/main.js
@@ -2,11 +2,31 @@ let sectionLogin= document.getElementById("sectionLogin"); //declaración de var
 let padre= document.getElementById("padre");
 
 let div = document.createElement("div"); //estamos creando un elemento div 
+div.className = "inicioDeSesion"; //traigo una clase de CSS a javaScript
 div.innerHTML=  ` 
-        <img>
-        <h2> Inicio de sesión </h2>
-        <div>
-            <form></form>
+        <div> <img src="../Imagenes/LandingImg/3779577.png"> </div>
+        <div class="form">
+                <h2> Inicio de sesión </h2>
+                <form action="">
+                        <div>
+                                <input for="GET-name" type="email" name="name" placeholder="Correo electrónico">
+                        <div>
+                        </div>
+                                <input for="GET-password" type="password" name="password" placeholder="Contraseña">
+                        </div>
+                        <div class="recordarme">
+                                <span><input type="checkbox" value "Recordarme"/> <p>Recordarme</p></span>
+                                <span> <p><a href="#">Olvidé mi contraseña </a></p> </span>
+                        </div>
+                        <div>
+                                <input type="submit" value="Iniciar Sesión" />
+                        </div>
+                        <div class="registrate">
+                                <span><p>¿No tienes una cuenta?</p> </span>
+                                <span><a href="#"> Regístrate.</a> </span>
+                        </div>
+                        
+                </form>
         </div>
 `; // .innerHTML lo utilizamos para crear dentro de la variable div etiquetas de HTML 
 
@@ -16,7 +36,7 @@ let divButton = document.createElement("div");
 
 divButton.innerHTML= `
 
-        <button type="submit"></button>
+        
 
 `;
 
