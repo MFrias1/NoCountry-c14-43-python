7e429af3d3bb67516bca8aac55a20eb495091f01
almubaDev <soy@almuba.dev>
2023-10-17 11:54:45 -0300

    Proceso de autenticacion

--- routers/user_router.py
+++ routers/user_router.py
@@ -3,6 +3,7 @@ from fastapi.responses import JSONResponse
 from config.database import Session
 from models.user_model import User
 from schemas.user_schema import CreateUser, CreateUserOut, LoginUser
+from services.user_service import UserService
 import json
 
 
@@ -11,13 +12,7 @@ user_router = APIRouter()
 @user_router.post('/create_user', status_code=201, response_model=CreateUserOut)
 def create_user(user: CreateUser) -> CreateUserOut:
     db =  Session()
-    new_user = User(**user.model_dump())
-    db.add(new_user)
-    db.commit() 
-    db.refresh(new_user) 
-    data = new_user.__dict__
-    #print(json.dump(data, indent=4))
-    data = {key: value for key, value in data.items() if not key.startswith("_") }
+    data = UserService(db).post_register_user(user)
     content = {
         'message':'Registro exitoso',
         'data' : data
