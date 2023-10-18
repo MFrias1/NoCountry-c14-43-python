529228d9867174cae0031879aadf737a9862b822
almubaDev <soy@almuba.dev>
2023-10-18 17:39:17 -0300

    Login funcional

--- Back/middlewares/authentications.py
+++ Back/middlewares/authentications.py
@@ -1,7 +1,8 @@
 import bcrypt
 
+salt = bcrypt.gensalt()
 def hash_password(password: str) -> str:
-    salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode('utf-8'), salt)
     return hashed_password.decode('utf-8')
+
 
