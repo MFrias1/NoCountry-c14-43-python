d82d1d99bd5c021c7863d39786ca17ad93b7e6f2
Daniel CAZABAT <dacazabatqgmail.com>
2023-10-19 09:39:02 -0300

    Railway

--- Back/config/database.py
+++ Back/config/database.py
@@ -6,7 +6,9 @@ from sqlalchemy.ext.declarative import declarative_base
 # sqlite_file_name = '../database.sqlite'
 # base_dir = os.path.dirname(os.path.realpath(__file__))
 
-database_url = "postgresql://postgres:pass@localhost:5432/recicla_ando"
+cnx_railway = "postgresql://postgres:jNCGUvEUz9ODFHsNYzkL@containers-us-west-203.railway.app:7565/railway"
+cnx_localhost = "postgresql://postgres:pass@localhost:5432/recicla_ando" 
+database_url = cnx_railway
 
 engine = create_engine(database_url, echo=True)
 
