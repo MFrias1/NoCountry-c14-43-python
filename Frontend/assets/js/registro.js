db804ad57fa94397040c2b3db0083a8d58aba91d
LeanCano <leandrocano24@gmail.com>
2023-10-22 22:57:20 -0500

    enlacevalidacionregistro

--- Frontend/assets/js/registro.js
+++ Frontend/assets/js/registro.js
@@ -1,51 +1,54 @@
-async function Post(event) {
-  event.preventDefault();
- // Obtener los datos del formulario
- const nombre = document.querySelector('input[name="nombre"]').value;
- const apellido = document.querySelector('input[name="apellido"]').value;
- const pais = document.querySelector('input[name="pais"]').value;
- const correo = document.querySelector('input[name="correo"]').value;
- const contrasenia = document.querySelector('input[name="contrasenia"]').value;
- const confirmarContrasenia = document.querySelector('input[name="confirmar-contrasenia"]').value;
+import './validaciones'
 
- // Verificar que las contraseñas coincidan
- if (contrasenia !== confirmarContrasenia) {
-   alert('Las contraseñas no coinciden');
-   return;
- }
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector('#register_form');
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
 
- // Crear un objeto con los datos del usuario
- const userData = {
-   first_name: nombre,
-   last_name: apellido,
-   country: pais,
-   email: correo,
-   password: contrasenia,
-   coins: 0
- };
+    // Validación de los campos
+    const nombre = form.querySelector('input[name="nombre"]').value;
+    const apellido = form.querySelector('input[name="apellido"]').value;
+    const pais = form.querySelector('input[name="pais"]').value;
+    const correo = form.querySelector('input[name="correo"]').value;
+    const contrasenia = form.querySelector('input[name="contrasenia"]').value;
+    const confirmarContrasenia = form.querySelector('input[name="confirmar-contrasenia"]').value;
 
- // Realizar una solicitud POST al servidor utilizando la Fetch API
- console.log('estoyadentro');
- await fetch('https://nocountry-api.onrender.com/create_user', {
-   method: 'POST',
-   headers: {
-     'Content-Type': 'application/json',
-   },
-   body: JSON.stringify(userData),
- })
-   .then(response => response.json())
-   .then(data => {
-     // Manejar la respuesta del servidor
-     console.log(data);
-     if (data.success) {
-       // Registro exitoso, puedes redirigir al usuario a una página de inicio de sesión
-       //window.location.href = 'iniciodesesion.html';
-     } else {
-       // Mostrar un mensaje de error en caso de fallo en el registro
-       alert('Error en el registro: ' + data.message);
-     }
-   })
-   .catch(error => {
-     console.error('Error de red:', error);
-   });
-}
+    if (contrasenia !== confirmarContrasenia) {
+      alert('Las contraseñas no coinciden');
+      return;
+    }
+
+    // Crear un objeto con los datos del usuario
+    const userData = {
+      first_name: nombre,
+      last_name: apellido,
+      country: pais,
+      email: correo,
+      password: contrasenia,
+      coins: 0
+    };
+
+    // Realizar una solicitud POST al servidor utilizando la Fetch API
+    console.log('Estoy dentro del formulario');
+    fetch('https://nocountry-api.onrender.com/create_user', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userData),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        if (data.success) {
+          // Registro exitoso, puedes redirigir al usuario a una página de inicio de sesión
+          // window.location.href = 'iniciodesesion.html';
+        } else {
+          alert('Error en el registro: ' + data.message);
+        }
+      })
+      .catch(error => {
+        console.error('Error de red:', error);
+      });
+  });
+});
---
+++ Frontend/assets/js/validaciones.js
@@ -0,0 +1,125 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Define una función para validar los campos con expresiones regulares y mensajes personalizados
+  function validateField(field, regex, customMessage) {
+    field.addEventListener("blur", function (e) {
+      const fieldValue = e.target.value;
+      const errorElement = document.getElementById(`${e.target.name}-error`);
+      
+      if (fieldValue.length === 0) {
+        e.target.classList.add("invalid");
+        errorElement.textContent = "Campo requerido";
+      } else if (!regex.test(fieldValue)) {
+        e.target.classList.add("invalid");
+        errorElement.textContent = customMessage;
+      } else {
+        e.target.classList.remove("invalid");
+        errorElement.textContent = "";
+      }
+    });
+  }
+
+  const nameRegex = /^[A-Za-zÁ-ú ]{3,}$/;
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  const passwordRegex = /^(?=.*\d)(?=.*[a-zA-Z]).{8,16}$/;
+
+  const nombreCampo = document.querySelector("[name=nombre]");
+  const apellidoCampo = document.querySelector("[name=apellido]");
+  const paisCampo = document.querySelector("[name=pais]");
+  const correoCampo = document.querySelector("[name=correo]");
+  const contraseniaCampo = document.querySelector("[name=contrasenia]");
+  const confirmarContraseniaCampo = document.querySelector("[name=confirmar-contrasenia]");
+  const form = document.querySelector('#register_form');
+
+  if (nombreCampo) {
+    validateField(nombreCampo, nameRegex, "Por favor, ingresa un nombre válido.");
+  }
+
+  if (apellidoCampo) {
+    validateField(apellidoCampo, nameRegex, "Por favor, ingresa un apellido válido.");
+  }
+
+  if (paisCampo) {
+    validateField(paisCampo, nameRegex, "Por favor, ingresa un país válido.");
+  }
+
+  if (correoCampo) {
+    validateField(correoCampo, emailRegex, "Por favor, ingresa un correo válido.");
+  }
+
+  if (contraseniaCampo) {
+    validateField(contraseniaCampo, passwordRegex, "La contraseña debe contener al menos 8 caracteres con números y letras.");
+  }
+
+  if (confirmarContraseniaCampo) {
+    validateField(confirmarContraseniaCampo, passwordRegex, "La confirmación de contraseña debe contener al menos 8 caracteres con números y letras.");
+    confirmarContraseniaCampo.addEventListener("blur", function (e) {
+      const passwordValue = contraseniaCampo.value;
+      const confirmValue = e.target.value;
+      const errorElement = document.getElementById(`${e.target.name}-error`);
+      
+      if (passwordValue !== confirmValue) {
+        e.target.classList.add("invalid");
+        errorElement.textContent = "Ambas contraseñas son diferentes";
+      }
+    });
+  }
+
+  form.addEventListener('submit', function (event) {
+    event.preventDefault();
+  
+    // Verificar si hay errores de validación en los campos
+    let hasErrors = false;
+  
+    if (nombreCampo.value.trim() === '' || !nameRegex.test(nombreCampo.value)) {
+      nombreCampo.classList.add('invalid');
+      document.getElementById('nombre-error').textContent = "Por favor, ingresa un nombre válido.";
+      hasErrors = true;
+    }
+  
+    if (apellidoCampo.value.trim() === '' || !nameRegex.test(apellidoCampo.value)) {
+      apellidoCampo.classList.add('invalid');
+      document.getElementById('apellido-error').textContent = "Por favor, ingresa un apellido válido.";
+      hasErrors = true;
+    }
+  
+    if (paisCampo.value.trim() === '' || !nameRegex.test(paisCampo.value)) {
+      paisCampo.classList.add('invalid');
+      document.getElementById('pais-error').textContent = "Por favor, ingresa un país válido.";
+      hasErrors = true;
+    }
+  
+    if (correoCampo.value.trim() === '' || !emailRegex.test(correoCampo.value)) {
+      correoCampo.classList.add('invalid');
+      document.getElementById('correo-error').textContent = "Por favor, ingresa un correo válido.";
+      hasErrors = true;
+    }
+  
+    if (contraseniaCampo.value.trim() === '' || !passwordRegex.test(contraseniaCampo.value)) {
+      contraseniaCampo.classList.add('invalid');
+      document.getElementById('contrasenia-error').textContent = "La contraseña debe contener al menos 8 caracteres con números y letras.";
+      hasErrors = true;
+    }
+  
+    if (confirmarContraseniaCampo.value.trim() === '' || confirmarContraseniaCampo.value !== contraseniaCampo.value) {
+      confirmarContraseniaCampo.classList.add('invalid');
+      document.getElementById('confirmar-contrasenia-error').textContent = "La confirmación de contraseña debe contener al menos 8 caracteres con números y letras.";
+      hasErrors = true;
+    }
+  
+    if (!hasErrors) {
+      // Si no hay errores, puedes continuar con el envío del formulario
+      // Aquí puedes agregar la lógica para enviar los datos al servidor
+    }
+  });
+  
+});
+
+
+
+
+
+
+
+
+
+
