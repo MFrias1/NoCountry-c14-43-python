6e0d8bfea068fbeb6c35196fabc8a0de2d212626
almubaDev <soy@almuba.dev>
2023-10-24 11:06:14 -0300

    Cambio direccion base de datos

--- Back/config/database.py
+++ Back/config/database.py
@@ -9,7 +9,7 @@ from sqlalchemy.ext.declarative import declarative_base
 cnx_railway = "postgresql://postgres:jNCGUvEUz9ODFHsNYzkL@containers-us-west-203.railway.app:7565/railway"
 cnx_localhost = "postgresql://postgres:root@localhost:5432/recicla_ando" 
 cnx_render = "postgresql://postgress:DXGBUR6oLx5WuQAR8ox6zFC6ZVtns36X@dpg-ckonvb7kc2qc73eprmu0-a/recicla_ando" 
-database_url = cnx_localhost
+database_url = cnx_railway
 
 
 engine = create_engine(database_url, echo=True)
