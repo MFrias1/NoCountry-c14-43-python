9095c7250e601dfb92aea4ca8eea06c8b1dcc65b
mayra <m.frias09@hotmail.com>
2023-10-24 20:56:31 -0300

    modal

--- Frontend/assets/js/recompensas.js
+++ Frontend/assets/js/recompensas.js
@@ -1,12 +1,15 @@
 let rankingBoton = document.getElementById('rankingBoton');
 let modal = document.getElementById('modal');
+let modalPopUp = document.getElementById('modalPopUp')
 let cierreBoton = document.getElementById('close');
 
 rankingBoton.addEventListener('click', () => {
-    modal.style.display = 'block';
+    modal.style.display='flex';
+    modalPopUp.style.display = 'block';
 });
 
 cierreBoton.addEventListener('click', () => {
     modal.style.display = 'none';
+    modalPopUp.style.display = 'none';
 });
 
