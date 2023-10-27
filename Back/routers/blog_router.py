4f971ba41d21bb53c4374675c745442d161c5c08
Alejandra <aleja78918@hotmail.com>
2023-10-26 21:34:18 -0500

    rutas blog

--- Back/routers/blog_router.py
+++ Back/routers/blog_router.py
@@ -6,12 +6,14 @@ from typing import List
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from services.blog_service import BlogService
+from typing import List
+from schemas.blog_schema import *
 
 
 blog_router = APIRouter()
 oauth2_scheme = OAuth2PasswordBearer('/token')
 
-@blog_router.post("/create_posts", status_code=201, response_model=CreateBlog)
+@blog_router.post('/create_posts', status_code=201, response_model=CreateBlog)
 def create_publication(create_publication: CreateBlog):
     db = Session()
     data = BlogService(db).create_publication(create_publication)
@@ -21,3 +23,32 @@ def create_publication(create_publication: CreateBlog):
              'data':jsonable_encoder(data)}
     
     return JSONResponse(status_code=201, content=content)
+
+@blog_router.get('/posts_all', response_model=List[BlogGetAll], status_code=201)
+def list_posts() ->List[BlogGetAll]:
+    db = Session()
+    posts = BlogService(db).get_blog_all()
+    return JSONResponse(status_code=200, content=jsonable_encoder(posts))
+
+@blog_router.get('/posts/{id}',status_code=200, response_model=BlogForId)
+def get_posts_id(id:int):
+    db = Session()
+    post = BlogService(db).get_blog_for_id(id)
+    return JSONResponse(status_code=200, content=jsonable_encoder(post))
+
+@blog_router.put('/posts/update_blog/{id}', status_code=201, response_model=dict)
+def update_posts(id:int,posts:UpdateInfoBlog):
+    db = Session()
+    result = BlogService(db).blog_update(id,posts)
+    if not result:
+        return JSONResponse(status_code=404, content={'msg':'post no encontrado'})
+    return JSONResponse(status_code=201, content={'msg':'post actualizado con éxito'})
+
+@blog_router.delete('/posts/delete_blog{id}', status_code=201, response_model=dict)
+def delete_posts(id:int,posts:DeleteBlog):
+    db = Session()
+    result = BlogService(db).blog_delete(id,posts)
+    if not result:
+        return JSONResponse(status_code=404, content={'msg':'post no encontrado'})
+    return JSONResponse(status_code=201, content={'msg':'post eliminado con éxito'})
+    
