ed3eeed3300d874fec99db471c41c55653c179a5
JoseJan21 <josejan303@gmail.com>
2023-10-28 01:58:50 -0300

    arreglo recicla_ando

--- Back/alembic/env.py
+++ Back/alembic/env.py
@@ -4,9 +4,7 @@ from sqlalchemy import engine_from_config
 from sqlalchemy import pool
 
 from alembic import context
-
-from models import user_model, blog_model, movement_model
-
+from models import user_model, blog_model, movement_model, prize_model
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -20,9 +18,10 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = user_model.User.metadata,  movement_model.Movement.metadata, blog_model.Blog.metadata
-
-
+target_metadata = user_model.User.metadata
+target_metadata = blog_model.Blog.metadata
+target_metadata = movement_model.Movement.metadata
+target_metadata = prize_model.Prize.metadata
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
