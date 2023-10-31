fe876c43647d07114882ee309e3ccb3c62def6f5
LeanCano <leandrocano24@gmail.com>
2023-10-31 10:02:28 -0500

    ajustes

--- Frontend/assets/js/navbar.js
+++ Frontend/assets/js/navbar.js
@@ -19,26 +19,9 @@ document.addEventListener("DOMContentLoaded", function () {
                             </div>
                         </div>
     `;
-    }
-    
-    /*if(window.location.href = 'login.html'){
-        botonInicioDeSesionIndex.style.display="none";
-        botonRegistro.style.display="none";
-    }*/
-
-    let userId= localStorage.getItem('userId');
-    let coins=localStorage.getItem('coins');
-    let first_name= localStorage.getItem('nombre');
-  
-    function logOut(){
-        const logOutImg= document.getElementById('logOut');
-        logOutImg.addEventListener('click', ()=> {
-            localStorage.removeItem('userId', 'coins','nombre');
-        });
     };
 
-    if(userId!=null && coins!=null && first_name!=null && (document.title="Bienvenido") ){
-       function navbarLogin(){
+    function navbarLogin(){
         navbar.innerHTML=`
         <a class="logo_navbar" href="./index.html"><img src="./assets/img/icon/logo.png" alt=""></a>
             <div class="navbar_enlaces_logIn">
@@ -51,7 +34,59 @@ document.addEventListener("DOMContentLoaded", function () {
                     <a href="#" class="logOut" id="logOut" ><img src="./assets/img/landingimg/logout.png" alt="" srcset="" ></a>
             </div>
         `;
-       };
+    };
+    
+    /*if(window.location.href = 'login.html'){
+        botonInicioDeSesionIndex.style.display="none";
+        botonRegistro.style.display="none";
+    }*/
+
+    let userId= localStorage.getItem('userId');
+    let coins=localStorage.getItem('coins');
+    let first_name= localStorage.getItem('nombre');
+
+    // logout
+    function logout(){
+        sessionStorage.removeItem('userId','coins','nombre');
+        window.location ='../index.html';
+    };
+
+    const logoutButton =document.getElementById("logOut");
+    if (logoutButton) {
+        logoutButton.addEventListener('click', logout);
+ }
+    
+    
+    //traigo monedero
+    let contenidoMonedero = document.getElementById("contenidoMonedero");
+
+    userId = parseInt(userId, 10); 
+        
+    //solicito datos a la API
+    if(userId){
+        
+        fetch(`https://nocountry-api.onrender.com/users/${userId}`)     
+        .then(respuestaApi => { 
+                if (!respuestaApi.ok) {
+                throw new Error('No se pudo obtener los datos del usuario');
+                }
+                //retorno la respuesta como un json.
+                return respuestaApi.json(); 
+        })//ejecuta la función flecha definida dentro del método .then(), con el argumento respuestaApi, si la solicitud se resuelve con éxito
+        .then(valorObtenido => {
+                // Muestra el contenido en el div
+                contenidoMonedero.innerText = valorObtenido.coins;
+        }) //si la solicitud no tiene éxito, el método .catch advetirá del error.
+        .catch(error => {
+                console.error('Error al obtener datos de la API: ', error);
+                alert('Error al obtener datos de la API.');
+        });
+
+
+
+    };
+    
+    if(userId!=null && coins!=null && first_name!=null && (document.title="Bienvenido") ){
        navbarLogin();
     }else if(userId!=null && coins!=null && first_name!=null && (document.title="blog")){
         navbarLogin();
@@ -61,11 +96,9 @@ document.addEventListener("DOMContentLoaded", function () {
        
     }else if(userId==null && coins==null && first_name==null && (document.title="Recicla-ando Bienvenidos")){
         navbarLogOut();
-        logOut();
-
+    
     }else if(userId==null && coins==null && first_name==null && (document.title="blog")){
         navbarLogOut(); 
-        logOut(); 
     };
 });
 
