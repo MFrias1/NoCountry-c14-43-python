b9d9ff2ea47e28e04d4edeafb622a1f89c24ae2b
mayra <m.frias09@hotmail.com>
2023-10-31 17:06:44 -0300

    emergente

--- Frontend/assets/js/juegoventanaemergente.js
+++ Frontend/assets/js/juegoventanaemergente.js
@@ -75,3 +75,12 @@ document.addEventListener('DOMContentLoaded',()=>{
     });;
 })
 
+let juegoInhabilitado= document.getElementById('juegoInhabilitado');
+juegoInhabilitado.addEventListener('click', ()=>{
+
+    Swal.fire({
+        title: '¡ESTAMOS EN MANTENIMIENTO!',
+        text: 'El juego se habilitará próximamente'
+      })
+
+});
--- Frontend/assets/js/recompensas.js
+++ Frontend/assets/js/recompensas.js
@@ -20,3 +20,12 @@ cierreBoton.addEventListener('click', () => {
     modalPopUp.style.display = 'none';
 });
 
+let juegoInhabilitado= document.getElementById('juegoInhabilitado');
+juegoInhabilitado.addEventListener('click', ()=>{
+
+    Swal.fire({
+        title: '¡ESTAMOS EN MANTENIMIENTO!',
+        text: 'El juego se habilitará próximamente'
+      })
+
+});
