bec97901719f0f9a5af3d09452cc57cf7f5ae6fb
daniel <zinnodaniel@gmail.com>
2023-11-02 14:39:30 -0300

    ultimosdetallesdelaapp

--- Frontend/assets/js/navbar.js
+++ Frontend/assets/js/navbar.js
@@ -23,6 +23,21 @@ document.addEventListener("DOMContentLoaded", function () {
     `;
     };
 
+    function navbarLogOutBlog() {
+        navbar.innerHTML= `
+        <a class="logo_navbar" href=""><img src="./assets/img/icon/logo.png" alt=""></a>
+                        <div class="navbar_enlaces">
+                            <div>
+                                <a id="botonInicioDeSesionIndex"  href="./index.html">Inicio</a><!--inicio de sesión-->
+                            </div>
+                            <div class="banner_button">
+                                <a id="botonInicioDeSesionIndex" class="banner_button_login" href="./iniciodesesion.html">Ingresar</a><!--inicio de sesión-->
+                                <a id="botonRegistro" class="banner_button_signUp" href="./registro.html">Registrarse</a><!--Registrarse>-->
+                            </div>
+                        </div>
+    `;
+    };
+
    
     //Funcion que carga el navbar, cuando el usuario esta logueado
     function navbarLogin(){
@@ -47,25 +62,38 @@ document.addEventListener("DOMContentLoaded", function () {
             localStorage.clear(); // Limpiar el localStorage
             window.location.href = "./index.html"; // Redirigir a la página de inicio o a la página deseada después del logout
         });
-         
-       
     };
 
+    function navbarLoginBlog(){
+        navbar.innerHTML=`
+        <a class="logo_navbar" href="./index.html" ><img src="./assets/img/icon/logo.png" alt=""></a>
+            <div class="navbar_enlaces_logIn">
+                    <div class="navbarLogIn_banner_button">
+                        <a class="banner_button_login" href="./recompensas.html">Recompensas</a>
+                    </div>
+                    <div class="navbarLogIn_banner_button">
+                        <a class="banner_button_login" href="./blog.html">Blog</a>
+                    </div>
+                    <div class="anchoMonedero banner_button_monedero">
+                        <p>Monedero $ </p><p id="contenidoMonedero"></p>
+                    </div>
+                    <a href="" class="logOut" id="logOut" ><img src="./assets/img/landingimg/logout.png" alt="" srcset="" ></a>
+            </div>
+        `;
+
+        document.getElementById('logOut').addEventListener('click', function (event) {
+            event.preventDefault(); // Evitar el comportamiento predeterminado del enlace
+            localStorage.clear(); // Limpiar el localStorage
+            window.location.href = "./index.html"; // Redirigir a la página de inicio o a la página deseada después del logout
+        });
+    } 
+
     // se guarda en distintas variables lo contenido en el localstorage
-
-
     let userId= localStorage.getItem('userId');
     let coins=localStorage.getItem('coins');
     let first_name= localStorage.getItem('nombre');
-
-
-
-
-    
     
     // se convierte el userId en interger, ya que en la api esta de tipo de variable interger
-   
-
     userId = parseInt(userId, 10); 
         
     //solicito datos a la API
@@ -88,15 +116,39 @@ document.addEventListener("DOMContentLoaded", function () {
                 console.error('Error al obtener datos de la API: ', error);
                 alert('Error al obtener datos de la API.');
         });
-
-
-
     };
     
+    // if (userId && coins && first_name) {
+    //     navbarLogin(); // Si hay información en el localStorage, el usuario está logueado
+    // } else if (userId && coins && first_name){
+    //     navbarLogOutBlog();
+    // }else if (userId==null && coins==null && first_name==null && document.title=='blog'){
+    //     navbarLogOutBlog();
+    // }else {
+    //     navbarLogOut(); // Si no hay información, el usuario está deslogueado
+       
+    // }
+
+    // if (userId && coins && first_name) {
+    //     navbarLogin(); // Si hay información en el localStorage, el usuario está logueado
+    // } else if (userId === null && coins === null && first_name === null && document.title==='blog') {
+    //     navbarLogOutBlog(); // El usuario está deslogueado y está en la página del Blog
+    // } else {
+    //     navbarLogOut(); // Si no hay información, el usuario está deslogueado en cualquier otra página
+    // }
+
     if (userId && coins && first_name) {
-        navbarLogin(); // Si hay información en el localStorage, el usuario está logueado
+        if (document.title === 'blog') {
+            navbarLoginBlog(); // El usuario está logueado y está en la página del Blog
+        } else {
+            navbarLogin(); // El usuario está logueado en cualquier otra página
+        }
     } else {
-        navbarLogOut(); // Si no hay información, el usuario está deslogueado
+        if (document.title === 'blog') {
+            navbarLogOutBlog(); // El usuario está logueado y está en la página del Blog
+        } else {
+            navbarLogOut(); // Si no hay información o no está logueado
+        }
     }
 });
 
